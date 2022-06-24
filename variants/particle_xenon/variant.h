5761f3686a90a4dc3b97d5443174bf2e2e00bfb4
Erich Bieri <erich@bieris.net>
2022-06-24 22:22:28 +0200

    Removed RTS/CTS defines for Xenon

--- variants/particle_xenon/variant.h
+++ variants/particle_xenon/variant.h
@@ -160,12 +160,8 @@ static const uint8_t AREF = PIN_AREF;
  */
 #define PIN_SERIAL1_RX         9
 #define PIN_SERIAL1_TX         10
-#define PIN_SERIAL1_CTS        4
-#define PIN_SERIAL1_RTS        3
 #define PIN_SERIAL2_RX         5
 #define PIN_SERIAL2_TX         4
-#define PIN_SERIAL2_CTS        6
-#define PIN_SERIAL2_RTS        8
 
 /*
  * SPI Interfaces
